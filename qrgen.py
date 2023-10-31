3e5962033cbb45977f0aacc5a6f9770118bd7e45
BupendraDhyan <bddwijegunawardhana@gmail.com>
2023-10-31 08:30:34 +0530

    Update qrgen.py

--- qrgen.py
+++ qrgen.py
@@ -8,3 +8,12 @@ def generate_qr_code(data, file_name):
         border=4,
     )
     qr.add_data(data)
+qr.make(fit=True)
+
+    qr_image = qr.make_image(fill_color="black", back_color="white")
+    qr_image.save(file_name)
+
+# Example usage:
+data_to_encode = "Hello, world!"
+output_file = "qr_code.png"
+generate_qr_code(data_to_encode, output_file)
